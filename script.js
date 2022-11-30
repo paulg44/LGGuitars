5d338c4e8b0a3269d2d220acc7ecf77b1d1052de
paulg44 <paulgarton1984@hotmail.com>
2022-11-30 20:56:30 +0000

    Prep for audio files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('playOneBtn').addEventListener('click', function () {
+  document.getElementById('playOne').play();
+});
+
+document.getElementById('playTwoBtn').addEventListener('click', function () {
+  document.getElementById('playTwo').play();
+});
