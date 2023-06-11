9cff8a0a02d5757d7a4bce0f486a421998497af9
paulg44 <paulgarton1984@hotmail.com>
2023-06-11 16:54:44 +0100

    netlify commit from vs code success

--- lg_guitars/src/App.js
+++ lg_guitars/src/App.js
@@ -6,12 +6,13 @@ import Home from "./pages/Homepage/Homepage";
 import Process from "./pages/Progress/Progress";
 import Contact from "./pages/Contact/Contact";
 
+// ABLE TO DEPLOY FROM COOMAND LINE TO NETLIFY BUT NEED TO SORT ROUTE PATHS ETC AS PAGES NOT SHOWING CORRECT OR AT ALL!!!!
+
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
-        <h1>Test</h1>
-        <Route path="/lg_guitars/src/pages/Homepage" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/lg_guitars/src/pages/Progress" element={<Process />} />
         <Route path="/lg_guitars/src/pages/Contact" element={<Contact />} />
       </Routes>
