7c752ddf86478e4e4008ad26cf3507d73ca24725
paulg44 <paulgarton1984@hotmail.com>
2023-07-13 21:24:34 +0100

    set up jest and write one test, others to follow

---
+++ lg_guitars/src/Components/HomepageComponents/GuitarCard/GuitarCard.test.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import GuitarCard from "./GuitarCard";
+
+// Test page renders
+test("page render", function () {
+  const { container } = render(
+    <MemoryRouter>
+      <GuitarCard />
+    </MemoryRouter>
+  );
+  // Test
+  expect(container).toBeInTheDocument();
+});
