ec1f5271eb6d5744c2df6051906c260d692fde2a
paulg44 <paulgarton1984@hotmail.com>
2023-08-12 19:42:25 +0100

    fix build problem on netlify

--- lg_guitars/src/App.js
+++ lg_guitars/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import {AnimatePresence } from "framer-motion"
+import { AnimatePresence } from "framer-motion";
 
 // Import Pages
 import Home from "./pages/Homepage/Homepage";
 import Process from "./pages/Progress/Progress";
 import Contact from "./pages/Contact/Contact";
-import NavBar from "./Components/NavBar/NavBar";
 
 // ABLE TO DEPLOY FROM COOMAND LINE TO NETLIFY BUT NEED TO SORT ROUTE PATHS ETC AS PAGES NOT SHOWING CORRECT OR AT ALL!!!!
 
@@ -23,14 +22,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/process-page" element={<Process />} />
         <Route path="/contact-page" element={<Contact />} />
-      </Routes>)
+      </Routes>
+    );
   }
 
   return (
     <BrowserRouter className="App">
       <LocationProvider>
-          <RoutesWithAnimation />
-        </LocationProvider>
+        <RoutesWithAnimation />
+      </LocationProvider>
     </BrowserRouter>
   );
 }
